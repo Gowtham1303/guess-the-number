f9277c2b8ceec44e9a6049f983320ca8f09ec8ca
Gowtham1303 <110476890+Gowtham1303@users.noreply.github.com>
2022-10-11 19:57:12 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,6 @@
+// Create login Function here.
+function login() {
+    player_name = document.getElementById("playername").ariaValueMax;
+    localStorage.setItem("player_name", player_name);
+    window.location = "gamepage.html";
+}
